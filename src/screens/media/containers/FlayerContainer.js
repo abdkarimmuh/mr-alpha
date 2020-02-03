593c40194b3a0edcff951f752cf4c07bcaa0ce17
Mutia Safari <mutiasafari97@gmail.com>
2020-02-04 01:59:06 +0700

    flyer and video content

--- src/screens/media/containers/FlayerContainer.js
+++ src/screens/media/containers/FlayerContainer.js
@@ -1,8 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import images from '@app/assets/images';
+import styles from '@app/assets/styles';
 import { Text } from '@app/components';
+import { Metrics } from '@app/themes';
+import React, { Component } from 'react';
+import { Image, ScrollView, View } from 'react-native';
 
-const FlayerContainer = () => {
-	return <Text>Ini Menu Flayer</Text>;
-};
+const data = [
+	{
+		id: 1,
+		title: 'Anies-Sandi',
+		image: images.dummy.oneToOne,
+	},
+	{
+		id: 2,
+		title: 'Salam Sejahtera',
+		image: images.dummy.oneToOne,
+	},
+];
+
+class FlayerContainer extends Component {
+	render() {
+		return (
+			<ScrollView contentContainerStyle={{ padding: 20 }}>
+				{data.map(item => (
+					<View
+						key={item.id}
+						style={[
+							styles.shadowOn,
+							{ backgroundColor: 'white', marginBottom: 20 },
+						]}
+					>
+						<Text numberOfLines={1} bold style={{ margin: 16 }}>
+							{item.title}
+						</Text>
+						<Image
+							source={item.image}
+							style={{
+								width: Metrics.DEVICE_WIDTH - 40,
+								height: Metrics.DEVICE_WIDTH - 40,
+							}}
+						/>
+					</View>
+				))}
+			</ScrollView>
+		);
+	}
+}
 
 export default FlayerContainer;
--- src/screens/media/containers/VideoContainer.js
+++ src/screens/media/containers/VideoContainer.js
@@ -1,8 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import images from '@app/assets/images';
+import styles from '@app/assets/styles';
 import { Text } from '@app/components';
+import { Metrics } from '@app/themes';
+import React, { PureComponent } from 'react';
+import { Image, ScrollView, View } from 'react-native';
 
-const VideoContainer = () => {
-	return <Text>Ini Menu Video</Text>;
-};
+const data = [
+	{
+		id: 1,
+		title: 'Ayo Tonton Debat Cagub Cawagub DKI di Saluran',
+		video: images.dummy.sixteenToNine,
+	},
+	{
+		id: 2,
+		title: 'Ayo Tonton Debat Cagub Cawagub DKI',
+		video: images.dummy.sixteenToNine,
+	},
+];
+
+class VideoContainer extends PureComponent {
+	render() {
+		return (
+			<ScrollView contentContainerStyle={{ padding: 20 }}>
+				{data.map(item => (
+					<View
+						key={item.id}
+						style={[
+							styles.shadowOn,
+							{ backgroundColor: 'white', marginBottom: 20 },
+						]}
+					>
+						<Text numberOfLines={1} bold style={{ margin: 16 }}>
+							{item.title}
+						</Text>
+						<Image
+							source={images.dummy.sixteenToNine}
+							style={{
+								resizeMode: 'contain',
+								width: Metrics.DEVICE_WIDTH - 40,
+								height: ((Metrics.DEVICE_WIDTH - 40) * 9) / 16,
+							}}
+						/>
+					</View>
+				))}
+			</ScrollView>
+		);
+	}
+}
 
 export default VideoContainer;
